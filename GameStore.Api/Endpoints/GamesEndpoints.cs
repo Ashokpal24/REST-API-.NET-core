bcf606c7a9d75e5d9ad44c91c4a9add8eedffc1b
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-12-02 12:10:17 +0000

    refactored code to make extension class.

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,98 @@
+namespace GameStore.Api.Endpoints;
+using GameStore.Api.Entities;
+
+public static class GameEndpoints
+{
+    const string GetGameEndpointName = "GetGame";
+
+    static readonly List<Game> games = new()
+    {
+        new Game()
+        {
+            Id=1,
+            Name="Assassin Cred II",
+            Genre="Action/RPG",
+            Price=19.99M,
+            ReleaseDate=new DateTime(2008,11,23),
+            ImageUri="https://placehold.co/100"
+        },
+        new Game()
+        {
+            Id=2,
+            Name="God of War II Chains of olympus",
+            Genre="Action",
+            Price=29.99M,
+            ReleaseDate=new DateTime(2005,7,16),
+            ImageUri="https://placehold.co/100"
+        },
+        new Game()
+        {
+            Id=3,
+            Name="Metal Gear Solid V phatom pain",
+            Genre="Action/RPG",
+            Price=39.99M,
+            ReleaseDate=new DateTime(2015,3,9),
+            ImageUri="https://placehold.co/100"
+        }
+    };
+    public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/games").WithParameterValidation();
+
+        routes.MapGet("", () => "Hi There!");
+
+        group.MapGet("/", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            Game? game = games.Find(game => game.Id == id);
+
+            if (game is null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(game);
+
+        }).WithName(GetGameEndpointName);
+
+        group.MapPost("/", (Game game) =>
+        {
+            game.Id = games.Max(game => game.Id) + 1;
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+        });
+
+
+        group.MapPut("/{id}", (int id, Game updateGame) =>
+        {
+            Game? existingGame = games.Find(game => game.Id == id);
+            if (existingGame is null)
+            {
+                return Results.NotFound();
+            }
+
+            existingGame.Name = updateGame.Name;
+            existingGame.Genre = updateGame.Genre;
+            existingGame.Price = updateGame.Price;
+            existingGame.ReleaseDate = updateGame.ReleaseDate;
+            existingGame.ImageUri = updateGame.ImageUri;
+
+            return Results.NoContent();
+
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            Game? delGame = games.Find(game => game.Id == id);
+            if (delGame is not null)
+            {
+                games.Remove(delGame);
+            }
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+
+}
